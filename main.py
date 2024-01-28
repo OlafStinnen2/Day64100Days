415f2a85c93a317ea86eaea42ab472e6ef8f8b92
OlafStinnen2 <82947177+OlafStinnen2@users.noreply.github.com>
2024-01-28 16:22:42 +0000

    Solution to Day64

--- main.py
+++ main.py
@@ -1,33 +1,68 @@
-class bankaccount:
-  customer_id = None
+# Create a generic 'job' class.
+# The init method will store the details for name, salary and hours worked
+class job:
   name = None
-  value = None
+  salary = None
+  hoursWorked = None
 
-  def __init__(self, customer_id, name, value):
-    self.customer_id = customer_id
+  def __init__(self, name, salary, hoursWorked):
     self.name = name
-    self.value = value
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+
+#'job' will have another method that prints those details nicely.
+  def job(self):
+    print((f'''
+    Job type: {self.name}
+    Salary: {self.salary} €
+    Hours Worked: {self.hoursWorked} per week''')) 
+
+#The 'doctor' subclass should also include 'speciality' and 'years of experience'.
+class doctor(job):
+  def __init__(self, name, salary, hoursWorked, speciality, yearsOfExperience):
+    self.name = name
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.speciality = speciality
+    self.yearsOfExperience = yearsOfExperience
+
+  #The print functions for each sub-class should print this extra data.
+  def doctor(self):
+    print((f'''
+    Job type: {self.name}
+    Salary: {self.salary} €
+    Hours Worked: {self.hoursWorked} per week
+    Speciality: {self.speciality}
+    Years of Experience: {self.yearsOfExperience}
+    '''))
+
+#The 'teacher' subclass should also include 'subject' and 'position
+class teacher(job):
+  def __init__(self, name, salary, hoursWorked, subject, position):
+    self.name = name
+    self.salary = salary
+    self.hoursWorked = hoursWorked
+    self.subject = subject
+    self.position = position
+  
+    #The print functions for each sub-class should print this extra data.
+  def teacher(self):
+    print((f'''
+    Job type: {self.name}
+    Salary: {self.salary} €
+    Hours Worked: {self.hoursWorked} per week
+    Subject: {self.subject}
+    Position: {self.position}
+    '''))
 
 
-  def transfer(self, amount):
-    if amount >= 0:
-      print(self.value)
-      self.value += amount
-    else:
-      if self.value < abs(amount):
-        print("To little money on bank account")
-      else:
-        self.value += amount
-        
 
-        
+banker = job("Banker",8000, "40")
 
+banker.job()
 
+DrAngeli = doctor("Doctor", 5000, "80", "Hausarzt", 20)
+DrAngeli.doctor()
 
-Olaf_Start = bankaccount("Olaf100", "Olaf Stinnen",100.01)
-Olaf_Start.value += 100
-Olaf_Start.transfer(-300)
-print(Olaf_Start.value)
-
-
-
+Willi = teacher("Teacher", 3000, "40", "Mathematik", "Vertretung")
+Willi.teacher()
